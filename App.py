c43d5b5bc1939187750f6314e6ee86461928cda2
maisammahloom <kreamykrunches@gmail.com>
2023-02-24 23:01:24 +0500

    initial commit

---
+++ App.py
@@ -0,0 +1,26 @@
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_title="Kanmathi Revengers", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+
+
+ lottie_coding = load_lottieurl("https://lottiefiles.com/90428-fist-bump")
+
+
+st.subheader("Hi im Maisam :wave:")
+st.title("Learning to code")
+st.write("im just testing to see if this works")
+st.write("this website is fully not complete expecting for it to be complete around march 27")
+st.write("[link to tutorial>](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+
+with st.container():
+    st.write("---")
+    st.header("Contact me for progress")
+    st.write("kreamykrunches@gmail.com")
+    st.write("##")
+
+
